161865d4309540f51cad699f43e7cf55ad5fe267
Adrian Mostowski <adaxelx@o2.pl>
2019-08-09 22:43:37 +0200

    add about content

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -7,13 +7,18 @@ import img from '../../images/mostoski.png'
 
 const StyledAbout = styled.section`
     width: 100%;
-    height: 100vh;
     margin-top: ${({theme}) => theme.marginBox}
     display: flex;
     flex-direction: column;
     align-items: center;
 `
 
+const StyledText = styled.p`
+    margin-top: ${({theme}) => theme.marginContent}
+    font-size: ${({theme}) => theme.fontSize.m}
+    width: 90%;
+    text-align: justify;
+`
 
 const StyledImg = styled.img`
     width: 90%;
@@ -21,8 +26,12 @@ const StyledImg = styled.img`
 const About = () => {
     return(
         <StyledAbout>
-            <Title content="Kim jestem?"/>
+            <Title content="Who am I?"/>
             <StyledImg src={img}/>
+            <StyledText>I'm Adrian, i'm 19 and i'm passionate of webdevelopment and desing. Doing this since '18, however I had tried earlier some other things like C++. For something completely different I can say that I'm training Taekwon-Do since 2008(almost 11 years).</StyledText>
+            <StyledText>As webdeveloper i'm trying to keep my code clean and readable for every developer. For most of my projects I'm using React (It's my favourite tool) but I have good knowlege of typical JS, HTML and CSS. </StyledText>
+            <StyledText>Actually my plan for the future is doing more courses and keep trying to create better and better websites. I'm also starting soon studies on Politechnika Warszawska on Informatic profile.</StyledText>
+
         </StyledAbout>
     )
 }
