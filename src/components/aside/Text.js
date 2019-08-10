3e4d4ab100f7e409f2655f49acf28240a4621864
Adrian Mostowski <adaxelx@o2.pl>
2019-08-10 16:39:57 +0200

    add about animation

---
+++ src/components/aside/Text.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import styled from 'styled-components';
+import { TimelineMax } from 'gsap/TweenMax';
+
+const StyledText = styled.p`
+    margin-top: ${({theme}) => theme.marginContent}
+    font-size: ${({theme}) => theme.fontSize.m}
+    width: 90%;
+    text-align: justify;
+    transform: translateY(100%);
+    opacity: 0;
+`
+
+class Text extends React.Component{
+
+    state = {active: false}
+
+    ref = React.createRef()
+
+    animate = () => {
+        const {ref} = this
+        const {active} = this.state
+        const tl = new TimelineMax();
+        const it = ref.current;
+        if(!active){
+            tl.addLabel('show')
+                .to(it, .5, { y: '-20px', opacity: 1 })
+                .addLabel('move')
+                .to(it, .2, { y: '0px'});
+        }
+        this.setState({
+            active: true
+        })
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll)
+    }
+
+    componentWillUnmount(){
+        window.removeEventListener('scroll', this.handleScroll)
+    }
+
+    handleScroll = () =>{
+        const scrollV = window.scrollY
+        const { ref,animate } = this
+        if (ref.current !== undefined && ref.current !== null) {
+          const off = ref.current.offsetTop
+          if (off < scrollV + window.innerHeight - ref.current.offsetHeight) {
+                animate();
+          }
+        }
+    }
+
+    render(){
+        const {ref} = this
+        const {content} = this.props
+        return(
+            <StyledText ref={ref}>{content}</StyledText>
+        )
+    }
+}
+
+
+export default Text;
--- src/components/main/About.js
+++ src/components/main/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components'
 
 import Title from '../aside/Title'
+import Text from '../aside/Text'
 
 import img from '../../images/mostoski.png'
 
@@ -13,13 +14,6 @@ const StyledAbout = styled.section`
     align-items: center;
 `
 
-const StyledText = styled.p`
-    margin-top: ${({theme}) => theme.marginContent}
-    font-size: ${({theme}) => theme.fontSize.m}
-    width: 90%;
-    text-align: justify;
-`
-
 const StyledImg = styled.img`
     width: 90%;
 `
@@ -28,9 +22,9 @@ const About = () => {
         <StyledAbout>
             <Title content="Who am I?"/>
             <StyledImg src={img}/>
-            <StyledText>I'm Adrian, i'm 19 and i'm passionate of webdevelopment and desing. Doing this since '18, however I had tried earlier some other things like C++. For something completely different I can say that I'm training Taekwon-Do since 2008(almost 11 years).</StyledText>
-            <StyledText>As webdeveloper i'm trying to keep my code clean and readable for every developer. For most of my projects I'm using React (It's my favourite tool) but I have good knowlege of typical JS, HTML and CSS. </StyledText>
-            <StyledText>Actually my plan for the future is doing more courses and keep trying to create better and better websites. I'm also starting soon studies on Politechnika Warszawska on Informatic profile.</StyledText>
+            <Text content="I'm Adrian, i'm 19 and i'm passionate of webdevelopment and desing. Doing this since '18, however I had tried earlier some other things like C++. For something completely different I can say that I'm training Taekwon-Do since 2008(almost 11 years)."></Text>
+            <Text content="As webdeveloper i'm trying to keep my code clean and readable for every developer. For most of my projects I'm using React (It's my favourite tool) but I have good knowlege of typical JS, HTML and CSS. "></Text>
+            <Text content="Actually my plan for the future is doing more courses and keep trying to create better and better websites. I'm also starting soon studies on Politechnika Warszawska on Informatic profile."></Text>
 
         </StyledAbout>
     )
