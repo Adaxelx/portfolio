1feff578725307be882cf5f457a3979c691567c8
Adrian Mostowski <adaxelx@o2.pl>
2019-08-11 23:08:49 +0200

    Updates

---
+++ precache-manifest.00d0ac36594132dcaf00772e08901eb8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f5aa0834ad733bf74f31663d0606e82",
+    "url": "/Portfolio2/index.html"
+  },
+  {
+    "revision": "25e1ba5ebd3274b7aaff",
+    "url": "/Portfolio2/static/js/2.9771be70.chunk.js"
+  },
+  {
+    "revision": "9bfedbce02b24e0ec05b",
+    "url": "/Portfolio2/static/js/main.16807e2e.chunk.js"
+  },
+  {
+    "revision": "9d869cc5c6a35476c8a0",
+    "url": "/Portfolio2/static/js/runtime~main.32625e0a.js"
+  },
+  {
+    "revision": "87f15cfa644615c61ce14ecae9888fcb",
+    "url": "/Portfolio2/static/media/mostoski.87f15cfa.png"
+  },
+  {
+    "revision": "3b8b3dc74effd77d9888e9ab1db6e51d",
+    "url": "/Portfolio2/static/media/react-router-2.3b8b3dc7.png"
+  },
+  {
+    "revision": "8f34ff7d71a85cdc212599d72583bd96",
+    "url": "/Portfolio2/static/media/salon.8f34ff7d.png"
+  },
+  {
+    "revision": "2f37751167dda952861983c6c86af543",
+    "url": "/Portfolio2/static/media/windows.2f377511.png"
+  }
+]);
