bdecf9cb2645f5b6365bdff55ead6d6594a0cc9c
Adrian Mostowski <adaxelx@o2.pl>
2020-09-03 19:45:48 +0200

    refactor Projects

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,19 +1,12 @@
 import React from "react";
-import { GridHome, About, Contact } from "./components";
-
-/* main */
-
-// import Home from '../../views/Dashboard/components/GridHome/GridHome'
-// import About from '../../components/main/About'
-// import Projects from '../../components/main/Projects'
-// import Contact from '../../components/main/Contact'
-// import Footer from '../../components/main/Footer'
+import { GridHome, About, Contact, Projects } from "./components";
 
 const Dashboard = () => {
   return (
     <>
       <GridHome />
       <About />
+      <Projects />
       <Contact />
     </>
   );
